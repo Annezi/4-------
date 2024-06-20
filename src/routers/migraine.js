e2d46309595fc049aa146a5cb41887e1a72773b2
Anna Axmatova <ax.annushka@yandex.ru>
2024-06-20 16:25:00 +0300

    Update upload

--- src/routers/migraine.js
+++ src/routers/migraine.js
@@ -38,6 +38,36 @@ migraineRouter.post('/migraine', (request, response) => {
         });
 });
 
+//обновление списка
+migraineRouter.put('/migraine', (request, response) => {
+    response.writeHead(200, {
+        'Content-type': 'application/json'
+    });
+
+    let data = '';
+
+    request
+        .on('data', (chunck) => {
+            data += chunck;
+        })
+        .on('end', () => {
+            const url = new URL('http://127.0.0.1:8080' + request.url);
+            const queryId = parseInt(url.searchParams.get('id'));
+
+            const payload = JSON.parse(data);
+
+            const DBMigraine = new DBJson('./src/db/migraines.json');
+
+            const success = DBMigraine.updateById(queryId, payload);
+
+            if (success) {
+                response.end(JSON.stringify(DBMigraine.getAll()));
+            } else {
+                response.end('Не удалось обновить запись');
+            }
+        });
+});
+
 migraineRouter.delete('/migraine', (request, response) => {
     response.writeHead(200, {
         'Content-type': 'application/json'
